7ec78a9abcfe7f7b8a4ca3113d4455803aa98daa
Margesh6400 <margeshpolara@gmail.com>
2025-07-22 10:27:03 +0530

    Fix useEffect import error

--- src/components/MobileReturnPage.tsx
+++ src/components/MobileReturnPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { supabase } from '../lib/supabase';
 import { Database } from '../lib/supabase';
 import { ClientSelector } from './ClientSelector';
