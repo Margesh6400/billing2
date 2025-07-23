7594b699f65976bcf7bce332f61a9eabbf2a3f3f
aastha6400 <polaraaastha@gmail.com>
2025-07-23 14:46:18 +0530

    Added index.mobile.html

---
+++ android/app/src/main/java/com/nowheretech/centeringplates/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nowheretech.centeringplates;
+
+import android.os.Bundle;
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Additional mobile-specific initialization can go here
+        // For example: setting up push notifications, analytics, etc.
+    }
+}
